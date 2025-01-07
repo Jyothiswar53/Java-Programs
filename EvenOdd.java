64086e6df38314ebdf54a8165ecacf4d8a46980a
JYOTHISWAR <jyothiswar285@gmail.com>
2025-01-07 16:34:45 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        float a;
+        System.out.println("enter the number");
+        a=obj.nextFloat();
+        if(a%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
