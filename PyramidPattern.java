7e3512cbca28c75f0dbac3565890818ba564c1ea
JYOTHISWAR <jyothiswar285@gmail.com>
2025-07-14 22:46:16 +0530

    rename Pattern.java to PyramidPattern.java

--- PyramidPattern.java
+++ PyramidPattern.java
@@ -8,17 +8,14 @@ public class PyramidPattern {
         int rows = sc.nextInt();
 
         for (int i = 1; i <= rows; i++) {
-            // Print spaces
             for (int j = 1; j <= rows - i; j++) {
                 System.out.print(" ");
             }
 
-            // Print stars
             for (int k = 1; k <= 2 * i - 1; k++) {
                 System.out.print("*");
             }
 
-            // Move to next line
             System.out.println();
         }
 
